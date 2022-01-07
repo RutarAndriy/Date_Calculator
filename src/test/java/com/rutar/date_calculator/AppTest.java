e34e4859b54d00d4989273ca56f8bf06cbff3f52
RutarAndriy <RutarAndriy@gmail.com>
2022-01-07 18:19:31 +0200

    Generated project structure

---
+++ src/test/java/com/rutar/date_calculator/AppTest.java
@@ -0,0 +1,19 @@
+package com.rutar.date_calculator;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+// ............................................................................
+
+public class AppTest {
+
+///////////////////////////////////////////////////////////////////////////////
+
+@Test
+public void should_Answer_With_True()
+    { assertTrue(true); }
+
+///////////////////////////////////////////////////////////////////////////////
+
+}
